c4938279b96c55b88ab2be8c3be221678449d2d1
sin-alpha-pi <53899103+sin-alpha-pi@users.noreply.github.com>
2020-12-04 16:24:35 +0100

    forced calibration

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,30 @@
 #include <Arduino.h>
-#include <SparkFun_SCD30_Arduino_Library.h>
+#include <Wire.h>
+#include "SparkFun_SCD30_Arduino_Library.h" 
+SCD30 sensor;
 
-void setup() {
-  // put your setup code here, to run once:
+void setup()
+{
+  Serial.begin(115200);
+  Wire.begin(4, 5);
+  if (sensor.begin(Wire, false) != true)
+  {
+    Serial.println("not detected");
+    while (1);
+  }
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop(){
+  if (Serial.available()){
+    if (Serial.readStringUntil('\n') == "cal"){
+      Serial.println("CALIBRATION");
+      sensor.setAltitudeCompensation(409);
+      sensor.setAmbientPressure(1003);
+      sensor.setForcedRecalibrationFactor(400);
+    }
+  }
+    
+  if (sensor.dataAvailable()){
+    Serial.println(sensor.getCO2());
+  }
 }
